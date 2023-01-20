12a1e1ef7311694f99e2291a2afdeeeb13eb5099
Hugo <huleonardo19@gmail.com>
2023-01-20 17:44:32 -0300

    feat: user interface e user  model criadas e implementadas

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from 'sequelize';
+import IUserModel from '../../interfaces/IUserModel';
+import db from '.';
+
+class User extends Model implements IUserModel {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init(
+  {
+    id: { primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER },
+    username: DataTypes.STRING,
+    role: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
+
+export default User;
