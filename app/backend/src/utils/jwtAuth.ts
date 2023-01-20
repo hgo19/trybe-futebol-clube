7a9e741377ce958ca6f9da2867e7c4c1bd95e04b
Hugo <huleonardo19@gmail.com>
2023-01-20 19:25:50 -0300

    refactor: interface implementada a classe JwtAuth

--- app/backend/src/utils/jwtAuth.ts
+++ app/backend/src/utils/jwtAuth.ts
@@ -1,14 +1,15 @@
 import * as jwt from 'jsonwebtoken';
 import type { JwtPayload } from 'jsonwebtoken';
+import AuthMethods from '../interfaces/IAuthMethods';
 
-export default class JwtAuth {
+export default class JwtAuth implements AuthMethods {
   private _secret: string;
 
   constructor() {
     this._secret = process.env.JWT_SECRET || 'yourScretToken';
   }
 
-  public encodeToken = (email: string) => {
+  public encodeToken = (email: string): string => {
     try {
       const token = jwt.sign(email, this._secret, {
         expiresIn: '5d',
@@ -18,15 +19,17 @@ export default class JwtAuth {
       return token;
     } catch (error) {
       console.log(error);
+      throw new Error('Falha ao executar o método encodeToken.');
     }
   };
 
-  public decodeToken = (token: string) => {
+  public decodeToken = (token: string): JwtPayload => {
     try {
       const decoded = jwt.verify(token, this._secret) as JwtPayload;
       return decoded;
     } catch (error) {
       console.log(error);
+      throw new Error('Falha ao executar o método decodeToken.');
     }
   };
 }
