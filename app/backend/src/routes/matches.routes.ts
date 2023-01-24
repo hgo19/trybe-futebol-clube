5c659573cd27c7ec781092fa76fc41167ab87987
Hugo <huleonardo19@gmail.com>
2023-01-24 16:06:18 -0300

    feat: validação se time existe no BD ao criar a match.

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -4,12 +4,14 @@ import MatchesServices from '../services/MatchesService';
 import MatchSequelizeRepository from '../repository/MatchSequelizeRepository';
 import LoginValidations from '../middlewares/LoginValidations';
 import JwtAuth from '../utils/jwtAuth';
+import TeamSequelizeRepository from '../repository/TeamSequelizeRepository';
 
 const router = Router();
 
 const authMethods = new JwtAuth();
-const repository = new MatchSequelizeRepository();
-const service = new MatchesServices(repository);
+const teamRepository = new TeamSequelizeRepository();
+const matchRepository = new MatchSequelizeRepository();
+const service = new MatchesServices(matchRepository, teamRepository);
 const controller = new MatchController(service, authMethods);
 
 router.get('/', controller.getAll);
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,11 +1,14 @@
 import HttpException from '../utils/errorsHandler/HttpException';
 import IMatchBasic from '../interfaces/IMatchBasic';
 import { IMatch } from '../interfaces/IModels';
-import { IMatchRepository } from '../interfaces/IRepositories';
+import { IMatchRepository, ITeamRepository } from '../interfaces/IRepositories';
 import { FinishedMatch, IMatchesService } from '../interfaces/IServices';
 
 export default class MatchesServices implements IMatchesService<IMatch> {
-  constructor(private _matchRepository: IMatchRepository) {}
+  constructor(
+    private _matchRepository: IMatchRepository,
+    private _teamRepository: ITeamRepository,
+  ) {}
 
   async getAll(): Promise<IMatch[]> {
     const allMatches = await this._matchRepository.getAll();
@@ -22,6 +25,13 @@ export default class MatchesServices implements IMatchesService<IMatch> {
     const { homeTeamId, awayTeamId } = newMatch;
     if (homeTeamId === awayTeamId) {
       throw new HttpException('It is not possible to create a match with two equal teams', 422);
+    }
+
+    const findHomeTeam = await this._teamRepository.getById(`${homeTeamId}`);
+    const findAwayTeam = await this._teamRepository.getById(`${homeTeamId}`);
+
+    if (!findHomeTeam || !findAwayTeam) {
+      throw new HttpException('There is no team with such id!', 404);
     }
 
     const createdMatch = await this._matchRepository.insert(newMatch);
